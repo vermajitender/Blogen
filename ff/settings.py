3d086dccfa5c5f6d07c3846b32cebb302fa44441
Jitender Verma <vermajounty1999@gmail.com>
2019-05-29 16:38:23 +0530

    Deployed

--- ff/settings.py
+++ ff/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ggq30ccol*l216r!=$n5hxv=95az58
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['radiant-wildwood-63552.herokuapp.com','127.0.0.1']
 
 
 # Application definition
