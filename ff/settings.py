4a8b8e982469ec29654d1c96a373b1e58a8d8d9f
Jitender Verma <vermajounty1999@gmail.com>
2019-05-29 17:32:39 +0530

    Deployed

--- ff/settings.py
+++ ff/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '../static/',
+    '/static/',
 ]
 
 # Heroku: Update database configuration from $DATABASE_URL.
